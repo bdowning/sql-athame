075dec9bf7833925ad594d15cdc52bcc1ce28c55
Brian Downing <bdowning@lavos.net>
2020-05-22 01:21:58 -0500

    Fix up example.py

--- example.py
+++ example.py
@@ -1,38 +1,44 @@
-from sql_athame import SqlA, format as Q
+from sql_athame import sql
 
 
 def get_orders(query):
-    where = [Q("TRUE")]
+    where = []
 
     if "id" in query:
-        where.append(Q("id = {}", query["id"]))
+        where.append(sql("id = {}", query["id"]))
     if "eventId" in query:
-        where.append(Q("event_id = {}", query["eventId"]))
+        where.append(sql("event_id = {}", query["eventId"]))
     if "startTime" in query:
-        where.append(Q("start_time = {}", query["startTime"]))
+        where.append(sql("start_time = {}", query["startTime"]))
     if "from" in query:
-        where.append(Q("start_time >= {}", query["from"]))
+        where.append(sql("start_time >= {}", query["from"]))
     if "until" in query:
-        where.append(Q("start_time < {}", query["until"]))
+        where.append(sql("start_time < {}", query["until"]))
 
-    return Q("SELECT * FROM orders WHERE {}", Q(" AND ").join(where))
+    return sql("SELECT * FROM orders WHERE {}", sql.all(where))
 
 
 print(get_orders({}).query())
 # ('SELECT * FROM orders WHERE TRUE', [])
 
+print(list(get_orders({})))
+# ['SELECT * FROM orders WHERE TRUE']
+
 print(get_orders({"id": "xyzzy"}).query())
 # ('SELECT * FROM orders WHERE TRUE AND id = $1', ['xyzzy'])
 
+print(list(get_orders({"id": "xyzzy"})))
+# ['SELECT * FROM orders WHERE TRUE AND id = $1', 'xyzzy']
+
 print(
-    get_orders(
+    *get_orders(
         {"eventId": "plugh", "from": "2019-05-01", "until": "2019-08-26"}
-    ).query()
+    )
 )
-# ('SELECT * FROM orders WHERE TRUE AND event_id = $1 AND start_time >= $2 AND start_time < $3', ['plugh', '2019-05-01', '2019-08-26'])
+# SELECT * FROM orders WHERE TRUE AND event_id = $1 AND start_time >= $2 AND start_time < $3 ['plugh', '2019-05-01', '2019-08-26']
 
 
-superquery = Q(
+superquery = sql(
     """
     SELECT *
       FROM ({subquery}) sq
